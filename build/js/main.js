7c335555de6ae387585c795ac6356e19979b86a7
0dot77 <yty0706@gmail.com>
2023-01-29 19:04:00 +0900

    first

---
+++ build/js/main.js
@@ -0,0 +1,14 @@
+"use strict";
+let myName = 'Dave';
+let meaningOfLife;
+let isLoading;
+let album;
+myName = 'John';
+meaningOfLife = 42;
+isLoading = true;
+album = 1984;
+album = 'hello';
+album = true;
+const sum = (a, b) => {
+    return a + b;
+};
---
+++ src/main.ts
@@ -0,0 +1,15 @@
+let myName: string = 'Dave';
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any;
+
+myName = 'John';
+meaningOfLife = 42;
+isLoading = true;
+album = 1984;
+album = 'hello';
+album = true;
+
+const sum = (a: number, b: number) => {
+  return a + b;
+};
